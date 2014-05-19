cef55f3daf32562f9e18539e3f12c2b2c08431e9
Mitchell Smith <zephyrthenoble@gmail.com>
2014-05-18 21:50:06 -0400

    Fixed an error where the unique array was used before initialization

--- password_cracker.cpp
+++ password_cracker.cpp
@@ -26,26 +26,51 @@ int main () {
     ifstream myfile ("passwords.txt");
     map<char, map<char,int> > mapping;
 
-
+    //I used to do this loop while appending elements to the mapping
+    //this caused the array of unique elements to be uninitialized completely
+    //while using said array to insert into the mapping
+    //
+    //goes through all ASCII characters 32-126, and then appends \n to the end 
     for(int x = 0; x < len_unique; x++) {
-        char key = (char)(32+x);
-        uniques[x] = key;
-        cout <<key<<endl;
-        map<char, int> val;
-        for (int y = 0; y < len_unique; y++) {
-            cout<<"\t"<<uniques[y]<<endl;
-            val.emplace(uniques[y], 1);
-        }
-        val.emplace('\n', 1);
-        mapping.emplace(key, val);
+        uniques[x] = (char)(32+x);
     }
     uniques[len_unique] = '\n';
     len_unique++;
 
+
+    //for each character in uniques other than \n, create a mapping to all 
+    //characters in uniques including \n
+    //this should create len_unique-1 keys, each with a map for a value
+    //the internal map will have len_unique keys
+    for(int x = 0; x < len_unique-1; x++) {
+        char key = uniques[x];
+        cout <<key<<endl;
+        map<char, int> val;
+        for (int y = 0; y < len_unique; y++) {
+            cout<<"\t"<<uniques[y]<<endl;
+            if( uniques[y] == '\0') { cout << "found a null val " << y << endl; }
+            val.emplace(uniques[y], 1);
+        }
+        mapping.emplace(key, val);
+    }
+//NOTE: we don't need to have a mapping for \n to things because if we get \n, we should stop
+    
+/*
     //prints ASCII 32-126 and \n
     for(int x = 0; x < len_unique; x++) {
         cout <<uniques[x]<<endl;
     }
+    for(map<char, map<char, int> >::iterator it = mapping.begin(); it != mapping.end(); it++) {
+        char key = it->first;
+        map<char, int> val = it->second;
+        cout<<"key "<<key<<endl;
+        for (map<char, int>::iterator it2 = val.begin(); it2 != val.end(); it2++) {
+            char ikey = it2->first;
+            int ival = it2->second;
+            cout<<"\t"<<"value "<<ikey<< " " <<(int)(ikey)<< "\t"<< ival<<endl;
+        }
+    }
+  */          
 /*
     string line;
     if (myfile.is_open())
